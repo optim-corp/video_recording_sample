73b7a2452e469b0d0f1a87f63508b17105b07450
Sari Yamaguchi <sari.yamaguchi@optim.co.jp>
2021-07-06 13:01:45 +0900

    Create Android Project

---
+++ app/src/main/java/jp/co/optim/video_recording_sample/MainActivity.kt
@@ -0,0 +1,11 @@
+package jp.co.optim.video_recording_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
