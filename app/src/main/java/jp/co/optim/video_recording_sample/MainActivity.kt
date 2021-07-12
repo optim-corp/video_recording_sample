825ed93e0c46718f6a065a109c4f22fffbd57ce2
Sari Yamaguchi <sari.yamaguchi@optim.co.jp>
2021-07-12 13:12:55 +0900

    Modify UI for recording audio

--- app/src/main/java/jp/co/optim/video_recording_sample/MainActivity.kt
+++ app/src/main/java/jp/co/optim/video_recording_sample/MainActivity.kt
@@ -1,11 +1,94 @@
 package jp.co.optim.video_recording_sample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import jp.co.optim.video_recording_sample.databinding.ActivityMainBinding
+import jp.co.optim.video_recording_sample.extensions.logI
+import jp.co.optim.video_recording_sample.read.MicAudioReader
+import jp.co.optim.video_recording_sample.record.MediaRecordManager
+import jp.co.optim.video_recording_sample.record.entity.AudioData
+import jp.co.optim.video_recording_sample.record.entity.RecordData
 
 class MainActivity : AppCompatActivity() {
+
+    private val runtimePermissions = arrayOf(
+        Manifest.permission.CAMERA,
+        Manifest.permission.RECORD_AUDIO
+    )
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var micAudioReader: MicAudioReader? = null
+
+    private var recordManager: MediaRecordManager? = null
+
+    private val requestMultiplePermissions =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+            for (entry in it.entries) {
+                if (!entry.value) {
+                    logI("Runtime permissions are denied: ${entry.key}")
+                    finish()
+                    return@registerForActivityResult
+                }
+            }
+            logI("All runtime permissions are granted.")
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonStart.setOnClickListener {
+            logI("Clicked start button.")
+            binding.buttonStart.isEnabled = false
+            binding.buttonStop.isEnabled = true
+            start()
+        }
+        binding.buttonStop.setOnClickListener {
+            logI("Clicked stop button.")
+            binding.buttonStart.isEnabled = true
+            binding.buttonStop.isEnabled = false
+            stop()
+        }
+
+        requestMultiplePermissions.launch(runtimePermissions)
+    }
+
+    private fun start() {
+        if (micAudioReader != null) {
+            logI("Already started.")
+            return
+        }
+        val dir = getExternalFilesDir(null)
+            ?: throw IllegalArgumentException("Cannot get parent dir.")
+        val recordData = RecordData.newAudioRecordData(dir, AudioData())
+        micAudioReader = MicAudioReader(recordData.audioData) {
+            recordManager?.inputAudioBytes(it)
+        }
+        recordManager = MediaRecordManager()
+
+        logI("Start recording.")
+        micAudioReader?.start()
+        recordManager?.prepare(recordData)
+        recordManager?.start()
+    }
+
+    private fun stop() {
+        if (micAudioReader == null) {
+            logI("Already stopped.")
+            return
+        }
+        logI("Stop recording.")
+        recordManager?.stop()
+        recordManager = null
+        micAudioReader?.stop()
+        micAudioReader = null
     }
 }
