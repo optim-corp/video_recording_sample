54fe755f7bd7fd0a2c77229ae5142ee1d79bc937
Sari Yamaguchi <sari.yamaguchi@optim.co.jp>
2021-07-13 23:32:39 +0900

    Add CameraVideoManager for camera capture.

---
+++ app/src/main/java/jp/co/optim/video_recording_sample/read/CameraCaptureRenderer.kt
@@ -0,0 +1,182 @@
+package jp.co.optim.video_recording_sample.read
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.hardware.camera2.*
+import android.hardware.camera2.params.OutputConfiguration
+import android.hardware.camera2.params.SessionConfiguration
+import android.os.Build
+import android.util.Size
+import android.view.Surface
+import android.view.TextureView
+import androidx.annotation.WorkerThread
+import androidx.core.app.ActivityCompat
+import jp.co.optim.video_recording_sample.extensions.logD
+import jp.co.optim.video_recording_sample.extensions.logI
+import jp.co.optim.video_recording_sample.record.entity.VideoData
+import kotlin.concurrent.thread
+
+class CameraCaptureRenderer(private val context: Context) {
+
+    private var textureView: TextureView? = null
+
+    private var cameraDevice: CameraDevice? = null
+
+    private var cameraCaptureSession: CameraCaptureSession? = null
+
+    private var captureRequest: CaptureRequest? = null
+
+    private var frameSize = Size(0, 0)
+
+    private var isOpened = false
+
+    private var isRendering = false
+
+    fun openCamera(view: TextureView, size: Size) {
+        textureView = view
+        frameSize = size
+
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            throw IllegalArgumentException("Camera permission is denied.")
+        }
+
+        val cameraManager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        cameraManager.openCamera(decideCameraId(cameraManager), deviceStateCallback, null)
+    }
+
+    fun closeCamera() {
+        isOpened = false
+
+        cameraCaptureSession?.close()
+        cameraDevice?.close()
+
+        textureView = null
+        cameraCaptureSession = null
+        cameraDevice = null
+        captureRequest = null
+    }
+
+    fun startRendering(
+        videoData: VideoData,
+        listener: (bitmap: Bitmap) -> Unit?
+    ) {
+        isRendering = true
+        thread { render(videoData.frameSize, videoData.frameRate, listener) }
+    }
+
+    fun stopRendering() {
+        isRendering = false
+    }
+
+    private fun decideCameraId(cameraManager: CameraManager): String {
+        val cameraIds = cameraManager.cameraIdList
+        var frontCameraId: String? = null
+        var backCameraId: String? = null
+        for (cameraId in cameraIds) {
+            when (cameraManager.getCameraCharacteristics(cameraId)
+                .get(CameraCharacteristics.LENS_FACING)) {
+                CameraCharacteristics.LENS_FACING_FRONT -> frontCameraId = frontCameraId ?: cameraId
+                CameraCharacteristics.LENS_FACING_BACK -> backCameraId = backCameraId ?: cameraId
+            }
+        }
+        return backCameraId ?: frontCameraId
+        ?: throw IllegalArgumentException("There are no cameras.")
+    }
+
+    private fun createCameraCaptureSession() {
+        logD("createCameraCaptureSession")
+        val texture = textureView?.surfaceTexture
+            ?: throw IllegalArgumentException("Cannot get surfaceTexture.")
+        texture.setDefaultBufferSize(frameSize.width, frameSize.height)
+
+        val surface = Surface(texture)
+
+        captureRequest = cameraDevice?.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)?.apply {
+            addTarget(surface)
+        }?.build()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            cameraDevice?.createCaptureSession(
+                SessionConfiguration(
+                    SessionConfiguration.SESSION_REGULAR,
+                    listOf(surface).map { OutputConfiguration(it) },
+                    context.mainExecutor,
+                    sessionStateCallback
+                )
+            )
+        } else {
+            cameraDevice?.createCaptureSession(
+                listOf(surface), sessionStateCallback, null)
+        }
+    }
+
+    @WorkerThread
+    private fun render(
+        size: Size,
+        frameRate: Int,
+        listener: (bitmap: Bitmap) -> Unit?
+    ) {
+        val intervalMillis = 1000L / frameRate
+        logI("Start rendering. timeInterval: $intervalMillis")
+        val dummyBitmap = let {
+            val bmp = Bitmap.createBitmap(size.width, size.height, Bitmap.Config.ARGB_8888)
+            val cv = Canvas(bmp)
+            val p = Paint().apply { color = Color.BLUE }
+            cv.drawRect(0f, 0f, size.width.toFloat(), size.height.toFloat(), p)
+            bmp
+        }
+        while (isRendering) {
+            val bitmap = if (isOpened) textureView?.getBitmap(frameSize.width, frameSize.height) else null
+            if (bitmap != null) {
+                listener(bitmap)
+                bitmap.recycle()
+            } else {
+                listener(dummyBitmap)
+            }
+            listener(dummyBitmap)
+
+            try {
+                Thread.sleep(intervalMillis)
+            } catch (e: InterruptedException) {
+                // Ignored.
+            }
+        }
+        dummyBitmap.recycle()
+        logI("End rendering.")
+    }
+
+    private val deviceStateCallback = object : CameraDevice.StateCallback() {
+        override fun onOpened(device: CameraDevice) {
+            cameraDevice = device
+            createCameraCaptureSession()
+        }
+        override fun onDisconnected(device: CameraDevice) {
+            device.close()
+            cameraDevice = null
+            isOpened = false
+        }
+        override fun onError(device: CameraDevice, error: Int) {
+            device.close()
+            cameraDevice = null
+            isOpened = false
+        }
+    }
+
+    private val sessionStateCallback = object : CameraCaptureSession.StateCallback() {
+        override fun onConfigured(session: CameraCaptureSession) {
+            cameraCaptureSession = session
+            cameraCaptureSession?.setRepeatingRequest(captureRequest!!, null, null)
+            isOpened = true
+        }
+        override fun onConfigureFailed(session: CameraCaptureSession) {
+            isOpened = false
+        }
+    }
+}
--- app/src/main/java/jp/co/optim/video_recording_sample/record/entity/VideoData.kt
+++ app/src/main/java/jp/co/optim/video_recording_sample/record/entity/VideoData.kt
@@ -1,23 +1,24 @@
 package jp.co.optim.video_recording_sample.record.entity
 
+import android.util.Size
+
 /**
  * 録画に関連する情報を有したデータクラス
- * @param width フレームの横サイズ
- * @param height フレームの縦サイズ
- * @param bitRate ビットレート
+ * @param frameSize フレームサイズ
  * @param frameRate フレームレート
  * @param frameInterval Iフレームの間隔
+ * @param bitRate ビットレート
  */
 data class VideoData(
-    val width: Int = 0,
-    val height: Int = 0,
-    val bitRate: Int = 480000,
+    val frameSize: Size = Size(0, 0),
     val frameRate: Int = 20,
     val frameInterval: Int = 10,
+    val bitRate: Int = frameSize.width * frameSize.height * frameRate / 8,
 ) {
     /**
      * 有効チェック
      */
     fun isAvailable(): Boolean =
-        (width > 0 && height > 0 && bitRate > 0 && frameRate > 0 && frameInterval > 0)
+        (frameSize.width > 0 && frameSize.height > 0
+                && frameRate > 0 && frameInterval > 0 && bitRate > 0)
 }
